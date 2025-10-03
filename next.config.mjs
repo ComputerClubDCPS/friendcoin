9e0cb1b8575de2fdbd93c8acfb33e6e965e3a64f
ComputerClubDCPS <184647740+ComputerClubDCPS@users.noreply.github.com>
2025-10-03 15:05:01 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -20,7 +20,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live https://*.sentry.io https://*.stack-auth.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live https://*.sentry.io https://*.stack-auth.com https://*.vercel.app https://sentry.io" ,
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: https:",
               "font-src 'self' data:",
